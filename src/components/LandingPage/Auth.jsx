42c43d98ab478e40b95ac69aefafb689e33b8e06
GeoSauer <geoffrey.sauer89@gmail.com>
2023-03-15 12:35:25 -0600

    implements Chakra UI

--- src/components/LandingPage/Auth.jsx
+++ src/components/LandingPage/Auth.jsx
@@ -1,41 +1,132 @@
-import {
-  // Navigate,
-  Outlet,
-  useNavigate,
-} from 'react-router-dom';
-// import { useUser } from '../../context/UserContext';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-import './Auth.css';
+// import './Auth.css';
+
+// export default function Auth() {
+
+//   return (
+//     <main className="auth">
+//       <button
+//         className="signUpButton"
+//         alt="create new account"
+//         title="Create New Account"
+//         onClick={() => {
+//           navigate('sign-up');
+//         }}
+//       >
+//         get started
+//       </button>
+//       <button
+//         className="signInButton"
+//         alt="sign in"
+//         title="Sign In"
+// onClick={() => {
+//   navigate('sign-in');
+// }}
+//       >
+//         I already have an account
+//       </button>
+//       <Outlet />
+//     </main>
+//   );
+// }
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 
 export default function Auth() {
-  // const user = useUser();
   const navigate = useNavigate();
-
-  // if (user) return <Navigate to="spell-list" />;
-
   return (
-    <main className="auth">
-      <button
-        className="signUpButton"
-        alt="create new account"
-        title="Create New Account"
-        onClick={() => {
-          navigate('sign-up');
-        }}
+    <Stack
+      minH={'100vh'}
+      direction={{ base: 'column', md: 'row' }}
+    >
+      <Flex
+        p={8}
+        flex={1}
+        align={'center'}
+        justify={'center'}
       >
-        get started
-      </button>
-      <button
-        className="signInButton"
-        alt="sign in"
-        title="Sign In"
-        onClick={() => {
-          navigate('sign-in');
-        }}
-      >
-        I already have an account
-      </button>
+        <Stack spacing={6} w={'full'} maxW={'lg'}>
+          <Heading
+            fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}
+          >
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: useBreakpointValue({
+                  base: '20%',
+                  md: '30%',
+                }),
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'blue.400',
+                zIndex: -1,
+              }}
+            >
+              Freelance
+            </Text>
+            <br />{' '}
+            <Text color={'blue.400'} as={'span'}>
+              Design Projects
+            </Text>{' '}
+          </Heading>
+          <Text
+            fontSize={{ base: 'md', lg: 'lg' }}
+            color={'gray.500'}
+          >
+            The project board is an exclusive resource for
+            contract work. Its perfect for freelancers,
+            agencies, and moonlighters.
+          </Text>
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            spacing={4}
+          >
+            <Button
+              rounded={'full'}
+              bg={'green.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}
+              onClick={() => {
+                navigate('sign-up');
+              }}
+            >
+              Let&apos;s Get Started!
+            </Button>
+            <Button
+              rounded={'full'}
+              onClick={() => {
+                navigate('sign-in');
+              }}
+            >
+              I Already Have An Account
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={'Login Image'}
+          objectFit={'cover'}
+          src={
+            'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+          }
+        />
+      </Flex>
       <Outlet />
-    </main>
+    </Stack>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import UserProvider from './context/UserContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
