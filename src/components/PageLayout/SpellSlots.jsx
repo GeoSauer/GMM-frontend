24cf6c58887588d6a266b825b196e97609523e2b
GeoSauer <geoffrey.sauer89@gmail.com>
2023-05-03 10:42:51 -0600

    refactors SpellDisplay into SpellSlots

--- src/components/PageLayout/SpellSlots.jsx
+++ src/components/PageLayout/SpellSlots.jsx
@@ -3,6 +3,7 @@ import { useCharacter } from '../../context/CharacterContext';
 
 export default function SpellSlots() {
   const { isOpen, onToggle } = useDisclosure();
+  const { characterInfo } = useCharacter();
   return (
     <>
       <Button w={'full'} rounded={'none'} onClick={onToggle}>
@@ -10,39 +11,32 @@ export default function SpellSlots() {
       </Button>
       <Collapse in={isOpen} animateOpacity>
         <Box p="40px" color="white" bg="teal.500" shadow="md">
-          <SpellSlotsDisplay />
+          {/* <SpellSlotsDisplay /> */}
+          <Flex w="full" justifyContent="space-around" alignItems="center">
+            {[...Array(9)].map((_, i) => {
+              const number = i + 1;
+              const spellsAvailableAtSlotLevel = characterInfo[`level${number}SpellSlots`];
+              return spellsAvailableAtSlotLevel ? (
+                <Box key={`key-${i}`} pos="relative">
+                  <Text>Level {number}:</Text>
+                  <Text
+                    pos="absolute"
+                    px={2}
+                    py={1}
+                    fontSize="xs"
+                    fontWeight="bold"
+                    color="red.100"
+                    bg="red.600"
+                    rounded="full"
+                  >
+                    {spellsAvailableAtSlotLevel}
+                  </Text>
+                </Box>
+              ) : null;
+            })}
+          </Flex>
         </Box>
       </Collapse>
     </>
   );
 }
-
-const SpellSlotsDisplay = () => {
-  const { characterInfo } = useCharacter();
-
-  return (
-    <Flex w="full" justifyContent="space-around" alignItems="center">
-      {[...Array(9)].map((_, i) => {
-        const number = i + 1;
-        const spellsAvailableAtSlotLevel = characterInfo[`level${number}SpellSlots`];
-        return spellsAvailableAtSlotLevel ? (
-          <Box key={`key-${i}`} pos="relative">
-            <Text>Level {number}:</Text>
-            <Text
-              pos="absolute"
-              px={2}
-              py={1}
-              fontSize="xs"
-              fontWeight="bold"
-              color="red.100"
-              bg="red.600"
-              rounded="full"
-            >
-              {spellsAvailableAtSlotLevel}
-            </Text>
-          </Box>
-        ) : null;
-      })}
-    </Flex>
-  );
-};
