223061b38c0615272b654ea51aed3982221f2a00
GeoSauer <geoffrey.sauer89@gmail.com>
2023-05-08 17:14:02 -0600

    cleans up AvailableSpellCard

--- src/components/Spells/AvailableSpellCard.jsx
+++ src/components/Spells/AvailableSpellCard.jsx
@@ -8,20 +8,11 @@ import {
   useDisclosure,
   VStack,
 } from '@chakra-ui/react';
-import { useLocation } from 'react-router-dom';
 import SpellDetail from './SpellDetail';
-import CastConcentrationSpellButton from '../Buttons/CastConcentrationSpellButton';
 import LearnSpellButton from '../Buttons/LearnSpellButton';
-import PrepareSpellButton from '../Buttons/PrepareSpellButton';
-import UnprepareSpellButton from '../Buttons/UnprepareSpellButton';
-import ForgetSpellButton from '../Buttons/ForgetSpellButton';
-import CastRitualSpellButton from '../Buttons/CastRitualSpellButton';
-import CastSpellButton from '../Buttons/CastSpellButton';
-import CastCantripButton from '../Buttons/CastCantripButton';
 
 export default function AvailableSpellCard({ spellDetails, spell }) {
   const { isOpen, onToggle } = useDisclosure();
-  const location = useLocation();
 
   return (
     <>
@@ -30,33 +21,7 @@ export default function AvailableSpellCard({ spellDetails, spell }) {
           <Heading size="md">{spell.name}</Heading>
           <Text>{spell.school}</Text>
         </Button>
-        <VStack>
-          {location.pathname === '/available-spells' && !spell.known && (
-            <LearnSpellButton spell={spell} />
-          )}
-
-          {location.pathname === '/known-spells' && !spell.prepared && (
-            <PrepareSpellButton spell={spell} />
-          )}
-          {location.pathname === '/known-spells' && <ForgetSpellButton spell={spell} />}
-
-          {location.pathname === '/prepared-spells' && spell.level === 0 && (
-            <CastCantripButton spell={spell} />
-          )}
-          {location.pathname === '/prepared-spells' && spellDetails.ritual && (
-            <CastRitualSpellButton spell={spell} />
-          )}
-          {location.pathname === '/prepared-spells' && spellDetails.concentration && (
-            <CastConcentrationSpellButton spell={spell} spellDetails={spellDetails} />
-          )}
-          {location.pathname === '/prepared-spells' &&
-            !spellDetails.concentration &&
-            !spellDetails.ritual &&
-            spell.level > 0 && <CastSpellButton spell={spell} spellDetails={spellDetails} />}
-          {location.pathname === '/prepared-spells' && spell.level > 0 && (
-            <UnprepareSpellButton spell={spell} />
-          )}
-        </VStack>
+        <VStack>{!spell.known && <LearnSpellButton spell={spell} />}</VStack>
       </HStack>
       <Collapse in={isOpen} animateOpacity>
         <Box
